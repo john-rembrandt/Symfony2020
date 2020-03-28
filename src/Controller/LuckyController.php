5d139009ebe01d28de57738f5453f4cc71d49a55
john-rembrandt <sk8mystic@gmail.com>
2020-03-28 21:47:22 +0100

    ajout des annotations dans class lucky et commenté routes.yaml

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,11 +1,17 @@
 <?php
 /* App/Controller/LuckyController.php */
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class LuckyController extends AbstractController
 {
+    /**
+    * @Route("/lucky/number")
+    */
     public function number()
     {
         $number = random_int(0, 100);
