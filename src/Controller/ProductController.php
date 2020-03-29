d58faebfe8fc8cdbcc376ba05319a9a762b5ca11
john-rembrandt <sk8mystic@gmail.com>
2020-03-30 00:46:32 +0100

    ajouer controller product et vue product

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Product;
+
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="create_product")
+     */
+    public function createProduct(): Response
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(1999);
+        $product->setDescription('Ergonomic and stylish!');
+        
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+        
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+        
+        return new Response('Saved new product with id '.$product->getId());
+    }
+}
