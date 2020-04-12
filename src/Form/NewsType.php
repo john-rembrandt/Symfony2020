d49b68029bc87d2095cf842df7208ed947322e31
john-rembrandt <sk8mystic@gmail.com>
2020-04-12 02:35:41 +0100

    make:crud sur entité : users,comments,news

---
+++ src/Form/NewsType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\News;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('auteur')
+            ->add('titre')
+            ->add('contenu')
+            ->add('date')
+            ->add('newsUser')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => News::class,
+        ]);
+    }
+}
