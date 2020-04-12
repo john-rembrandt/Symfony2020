41f2c52cccdcec874d38922052133f20f82ec152
john-rembrandt <sk8mystic@gmail.com>
2020-04-12 01:11:12 +0100

    ajout d pne classe user ainsi que sa table

---
+++ src/Entity/Comments.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentsRepository")
+ */
+class Comments
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\news", inversedBy="comments")
+     */
+    private $newsComments;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getNewsComments(): ?news
+    {
+        return $this->newsComments;
+    }
+
+    public function setNewsComments(?news $newsComments): self
+    {
+        $this->newsComments = $newsComments;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20200407211942.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200407211942 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE news DROP FOREIGN KEY news_ibfk_1');
+        $this->addSql('ALTER TABLE news ADD news_user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE news ADD CONSTRAINT FK_1DD39950F08ACF73 FOREIGN KEY (news_user_id) REFERENCES users (id)');
+        $this->addSql('CREATE INDEX IDX_1DD39950F08ACF73 ON news (news_user_id)');
+        $this->addSql('ALTER TABLE users DROP FOREIGN KEY users_ibfk_1');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE news DROP FOREIGN KEY FK_1DD39950F08ACF73');
+        $this->addSql('DROP INDEX IDX_1DD39950F08ACF73 ON news');
+        $this->addSql('ALTER TABLE news DROP news_user_id');
+        $this->addSql('ALTER TABLE news ADD CONSTRAINT news_ibfk_1 FOREIGN KEY (id) REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE users ADD CONSTRAINT users_ibfk_1 FOREIGN KEY (id) REFERENCES news (id) ON UPDATE CASCADE ON DELETE NO ACTION');
+    }
+}
